9968874cb3c670b198c0e2b999f0389fecf9238a
Georgie-GitHub <153370146+Georgie-GitHub@users.noreply.github.com>
2025-06-19 02:21:00 +0530

    Update App.jsx
    i added the vercel url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     fetchContacts();
   }, []);
   const fetchContacts = async () => {
-    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const response = await fetch("https://contacts-sepia.vercel.app/contacts");
     const data = await response.json();
     setContacts(data.contacts);
   };
