d5d7319410d704fe66bf96d0c501aa2a17a7dce7
Georgie-GitHub <153370146+Georgie-GitHub@users.noreply.github.com>
2025-06-19 02:22:11 +0530

    Update ContactList.jsx
    vercel url added

--- frontend/src/ContactList.jsx
+++ frontend/src/ContactList.jsx
@@ -7,7 +7,7 @@ const ContactList = ({ contacts, updateContact, updateCallback }) => {
         method: "DELETE",
       };
       const response = await fetch(
-        `http://127.0.0.1:5000/delete_contact/${id}`,
+        `https://contacts-sepia.vercel.app/delete_contact/${id}`,
         options
       );
       if (response.status === 200) {
